c94be3fd4905414a44f47edf2566de012cf8f2d5
camiDev <103083100+camila-pereyra@users.noreply.github.com>
2024-11-13 23:14:07 -0300

    Estilos de App e Imput. Agregue dos botones para aumentar o dismuir contador de numero. Cambio titulo principal de la app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     return (
         <>
             <div className="app-container">
-                <h1 className="app-tittle">APIs CataasAPI & Rick and Morty</h1>
+                <h1 className="app-tittle">APIs utilizadas: <span>Cataas</span> & <span>Rick and Morty</span></h1>
                 <div className="app-containerCard">
                     {!numberID ?
                         <Input formatNumberChose={formatNumberChose} />
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import "./Input.css"
 
 
-const Input = ({formatNumberChose}) => {
+const Input = ({ formatNumberChose }) => {
     const [inputNumber, setInputNumber] = useState(1)
 
     const handleClick = () => {
@@ -13,11 +13,24 @@ const Input = ({formatNumberChose}) => {
         setInputNumber(e.target.value)
     }
 
+    const addNumber = () => {
+        setInputNumber(inputNumber + 1)
+    }
+    const subtractNumber = () => {
+        inputNumber > 1 ? setInputNumber(inputNumber - 1) : setInputNumber(1)
+    }
+
     return (
         <div className="input-container">
             <p className="input-paragraph">Ingrese un numero del 1 al 826 para seleccionar a un personaje de Rick and Morty</p>
-            <input type="number" className="input-input" min={1} max={826} step={1} value={inputNumber} onChange={changeNumber} />
-            <button onClick={handleClick} >Ver</button>
+            <div className="input-counterPanel">
+                <button className="buttonAddOrSubtract" onClick={subtractNumber}>-</button>
+                <input type="number" className="input-input" min={1} max={826} step={1} value={inputNumber} onChange={changeNumber} />
+                <button onClick={addNumber} className="buttonAddOrSubtract">+</button>
+            </div>
+            <div className="effect-card">
+                <button className="service-button pulse" onClick={handleClick} >Ver</button>
+            </div>
         </div>
     )
 }
