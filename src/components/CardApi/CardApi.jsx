49e9fb238d28dbdf4ce23f5f47c962a3116e68c6
camiDev <103083100+camila-pereyra@users.noreply.github.com>
2024-11-14 21:29:17 -0300

    Se agrego mas informacion respecto al response de cada API, se modificaron estilos de CardApi

--- src/components/CardApi/CardApi.jsx
+++ src/components/CardApi/CardApi.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react"
 import "./CardApi.css"
 
 
-const CardApi = ({numberID}) => {
+const CardApi = ({ numberID }) => {
     const [rickAndMortyName, setRickAndMortyName] = useState()
+    const [rickAndMortyStatus, setRickAndMortyStatus] = useState()
+    const [rickAndMortySpecies, setRickAndMortySpecies] = useState()
     const [rickAndMortyImage, setRickAndMortyImage] = useState()
     const [cataasImage, setCataasImage] = useState()
 
@@ -13,8 +15,10 @@ const CardApi = ({numberID}) => {
             .then((data) => {
                 const firstName = data.name.split(' ')[0]
                 setRickAndMortyName(data.name)
+                setRickAndMortyStatus(data.status)
+                setRickAndMortySpecies(data.species)
                 setRickAndMortyImage(data.image)
-                fetch(`https://cataas.com/cat/says/Hello%20${firstName}`)
+                fetch(`https://cataas.com/cat/says/Hola%20${firstName}`)
                     .then(res => {
                         setCataasImage(res.url)
                     })
@@ -24,10 +28,16 @@ const CardApi = ({numberID}) => {
     return (
         <>
             <div className="api-container">
-                <strong className="api-name">I am {rickAndMortyName}</strong>
+                <strong className="api-used">Rick and Morty API</strong>
+                <strong className="api-name">Personaje elegido: {numberID}</strong>
+                <strong className="api-name">Nombre: {rickAndMortyName}</strong>
+                <strong className="api-name"> Estado: {rickAndMortyStatus}</strong>
+                <strong className="api-name"> Especie: {rickAndMortySpecies}</strong>
                 <img className="api-image" src={rickAndMortyImage} />
             </div>
             <div className="api-container">
+                <strong className="api-used">Cataas API</strong>
+                <strong className="api-name">Devuelve una imagen de un gato saludando al personaje elegido: {rickAndMortyName}</strong>
                 <img className="api-image" src={cataasImage} />
             </div>
         </>
